f30c11eeb8f232e1f71afba62f1e59a532738e6f
Hamszn <nicholaushamilton@gmail.com>
2024-07-23 21:37:35 -0500

    update code
    add all my coder

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+
+const {Circle, Square, Triangle} = require("./lib/shapes");
+
+class Svg{
+    constructor(){
+        this.textElement = ""
+        this.shapeElement = ""
+    }
+    render(){
+        return 
+    }
+}
